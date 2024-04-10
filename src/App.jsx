da65c7347bc3bdac39cd2cc37220e8434abdc9e0
Ndimofor <ndimaret@gmail.com>
2024-04-10 09:59:46 +0200

    add boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-violet-600">
+          Aretas & Mofak Collaboration
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
